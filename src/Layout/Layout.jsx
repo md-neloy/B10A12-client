fc98579770e36c1ebf5584cfc073236cab01b5f3
mhneloy <mhneloy708@gmail.com>
2025-02-19 17:50:13 +0600

    adding the icon in navbar for dark theme

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -3,11 +3,13 @@ import Navbar from "../Sharecomponent/Navbar";
 import Footer from "../Sharecomponent/Footer";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import useContexHooks from "../useHooks/useContexHooks";
 
 const Layout = () => {
   AOS.init();
+  const { togol } = useContexHooks();
   return (
-    <div>
+    <div className={`bg-${togol ? "transparent" : "[#111827]"}`}>
       <Navbar />
       <div className="min-h-[calc(100vh-360px)]">
         <Outlet />
